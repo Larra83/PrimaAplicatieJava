4c21b77bf357b8ac19a0f0ef27a79f18f55bd096
Larra83 <laura@peliniievents.ro>
2021-03-07 14:37:50 +0200

    nou comentariu la main vis-a-vis de prescurtare functia main

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -8,4 +8,9 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
+    /*
+    Tot printr-o prescurtare si anume prescurtarea main + dublu click
+    pe main in meniul aparut in mod implicit, vom introduce functia main
+     */
 }
+
