0df2d0cde4425ffadafee59a38bc58b51bc2d536
Larra83 <laura@peliniievents.ro>
2021-03-07 13:56:08 +0200

    Adaugare comentariu in Main pentru functia System.out.printl

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,7 +1,10 @@
 package ro.java.ctrln;
 
 public class Main {
-
+    /*Functia System.out.println("") poate fi chemata spre executare
+    cu o prescurtare de tipul sout si dublu click pe optiunea sout din
+    meniul ce apare in mod implicit care va introduce astfel System.out.println("")
+     */
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
