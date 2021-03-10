14c387d7f3a2e34783978183e30a1df2534dfac6
Larra83 <laura@peliniievents.ro>
2021-03-10 16:13:27 +0200

    am adaugat javadoc

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,27 +1,13 @@
 package ro.java.ctrln;
-    /*
-    Exemplu de comentariu pe mai multe linii:
-    In cazul FOR LOOP, WHILE si DO-WHILE loops se va face mereu incrementarea
-    variabilei counter/iterator pentru a evita un loop infinit, ce va
-    consuma memoria stack a calculatorului pana la epuizare si va genera eroare.
-     */
 
 public class Main {
-    /*Functia System.out.println("") poate fi chemata spre executare
-    cu o prescurtare de tipul sout si dublu click pe optiunea sout din
-    meniul ce apare in mod implicit care va introduce astfel System.out.println("")
+    /**
+     * metoda main este metoda apelata de masina virtuala Java care porneste o aplicatie Java
+     * putem sa transmitem argumente aditionale prin parametrul <b>args</b>
+     * @param args acesta este parametrul prin care transmitem argumente programului nostru
+     * @author Pelin Laura
      */
-    public static void main(/* args este parametrul functiei main */String[] args) {
-	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+    public static void main(String[] args) {
+        System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
-    /*
-    Tot printr-o prescurtare si anume prescurtarea main + dublu click
-    pe main in meniul aparut in mod implicit, vom introduce functia main
-     */
 }
-       /* Comentariile pe o linie se pot desparti cu enter,
-       formandu-se o noua linie comentariu,
-// iar tot ceea ce exista dupa locul unde am pus cursorul
-se transforma intr-o noua linie cu tasta Enter
-
-        */
